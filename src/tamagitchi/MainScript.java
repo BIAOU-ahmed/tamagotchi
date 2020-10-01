486aadf4de1fca21f252b68a59b81e3021e9e564
Biaou ahmed <ahmed.alidjinou@hotmail.com>
2020-10-01 17:31:30 +0200

    add all funtion

--- src/tamagitchi/MainScript.java
+++ src/tamagitchi/MainScript.java
@@ -1,11 +1,105 @@
 package tamagitchi;
 
+import outils.Clavier;
+
 public class MainScript {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
+		int action;
+		String tamagotchiType;
+		boolean corectType = false;
+		System.out.println("Bienvenue dans le jeux Tamagotchi !");
 		
+		System.out.println("Veuillez entrez le nom de votre tamagotchi");
+		String name = Clavier.lireString();
+		
+		System.out.println("Veuillez entrez la couleur de votre tamagotchi");
+		String skinColor = Clavier.lireString();
+		
+		System.out.println("Veuillez entrez la taile en cm de votre tamagotchi");
+		int height = Clavier.lireInt();
+		
+		System.out.println("Veuillez entrez la couleur du cheveux de votre tamagotchi");
+		String hairColor = Clavier.lireString();
+		
+		
+		do {
+			
+			System.out.println("Veuillez entrez le type  de votre tamagotchi parmi les suivante :");
+			System.out.println("Chat Chien Dragon");
+			tamagotchiType = Clavier.lireString();
+			if (tamagotchiType.equals("Chat") || tamagotchiType.equals("Chien") || tamagotchiType.equals("Dragon")) {
+				corectType = true;
+			}
+			
+		} while (corectType==false);
+		
+		Tamagotchi myTamagotchi = new Tamagotchi(name,skinColor,hairColor,tamagotchiType,height);
+		
+		myTamagotchi.show();
+		gameMenu();
+		action = Clavier.lireInt();
+		switch (action) {
+		case 1: {
+			myTamagotchi.eat();
+			break;
+		}
+		case 2: {
+			myTamagotchi.goToToilet();
+			break;
+		}
+		case 3: {
+			String newHairColor = Clavier.lireString();
+			myTamagotchi.setHairColor(newHairColor);
+			break;
+		}
+		case 4: {
+			myTamagotchi.rest();
+			break;
+		}
+		case 5: {
+			
+			myTamagotchi.getMood();
+			break;
+		}
+		case 6: {
+			
+			break;
+		}
+		case 7: {
+			
+			myTamagotchi.playSports();
+			break;
+		}
+		case 8: {
+			
+			myTamagotchi.takeOffMask();
+			break;
+		}
+		case 9: {
+			
+			myTamagotchi.wash();
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + action);
+		}
+		
+	}
+	
+	public static void gameMenu() {
+		System.out.println("\nVoici le menu du jeux :");
+		System.out.println("1: pour faire manger votre tamagotchi");
+		System.out.println("2: pour le faire aller au toilet");
+		System.out.println("3: pour changer la couleur de ses cheveux");
+		System.out.println("4: pour qu'il se repose");
+		System.out.println("5: pour qu'il exprime son humeur");
+		System.out.println("6: pour qu'il voit ses amis");
+		System.out.println("7: pour qu'il fasse du sport");
+		System.out.println("8: pour qu'il enleve sont masque");
+		System.out.println("9: pour qu'il se lave");
 	}
 
 }
--- src/tamagitchi/Tamagotchi.java
+++ src/tamagitchi/Tamagotchi.java
@@ -3,62 +3,110 @@ package tamagitchi;
 public class Tamagotchi {
 
 	String name;
-	String color;
+	String skinColor;
+	String mood = "Neutre";
 	String hairColor;
 	String tamagotchiType;
 	boolean masked;
 
-
+	int dirtLevel=0;
+	final int HEALTHMAXCAT=10;
+	final int HEALTHMAXDRAGON=20;
 	int height;
 	int health;
 	int lifeExpectancy;
-	int hungerLevel;
-	int tiredness;
-	
+	int hungerLevel = 0;
+	int tiredness = 0;
+
+	public Tamagotchi(String name, String skinColor, String hairColor, String tamagotchiType, int height) {
+		// TODO Auto-generated constructor stub
+		setName(name);
+		setSkinColor(skinColor);
+		setHairColor(hairColor);
+		setTamagotchiType(tamagotchiType);
+		setHeight(height);
+		if (tamagotchiType.equals("Dragon")) {
+
+			health = HEALTHMAXDRAGON;
+			lifeExpectancy = 15;
+			
+		} else if (tamagotchiType.equals("Chat") || tamagotchiType.equals("Chien")) {
+			health = HEALTHMAXCAT;
+			lifeExpectancy = 8;
+		}
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public String getColor() {
-		return color;
+
+	public String getSkinColor() {
+		return skinColor;
 	}
-	public void setColor(String color) {
-		this.color = color;
+
+	public void setSkinColor(String skinColor) {
+		this.skinColor = skinColor;
 	}
-	
+
 	public String getHairColor() {
 		return hairColor;
 	}
+
 	public void setHairColor(String hairColor) {
 		this.hairColor = hairColor;
 	}
+
 	
+	
+	public String getMood() {
+		return mood;
+	}
+
+	public void setMood(String mood) {
+		this.mood = mood;
+	}
+
 	public String getTamagotchiType() {
 		return tamagotchiType;
 	}
+
 	public void setTamagotchiType(String tamagotchiType) {
 		this.tamagotchiType = tamagotchiType;
 	}
-	
+
 	public int getHeight() {
 		return height;
 	}
-	
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	public int getHealth() {
 		return health;
 	}
 	
+	
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
 	public int getLifeExpectancy() {
 		return lifeExpectancy;
 	}
-	
+
 	public int getHungerLevel() {
 		return hungerLevel;
 	}
-	
+	public void setHungerLevel(int hungerLevel) {
+		this.hungerLevel = hungerLevel;
+	}
+
 	public int getTiredness() {
 		return tiredness;
 	}
@@ -66,14 +114,93 @@ public class Tamagotchi {
 	public boolean isMasked() {
 		return masked;
 	}
+
 	public void setMasked(boolean masked) {
 		this.masked = masked;
 	}
+
+	
+	public int getDirtLevel() {
+		return dirtLevel;
+	}
+
+	public void setDirtLevel(int dirtLevel) {
+		this.dirtLevel = dirtLevel;
+	}
+
 	
 	public void show() {
-		System.out.println();
+		System.out.println("Voici les caracteristique de votre tamagotchi :");
+		System.out.println("Nom : "+this.name);
+		System.out.println("Taille : "+this.height);
+		System.out.println("Type : "+this.tamagotchiType);
+		System.out.println("Couleur : "+this.skinColor);
+		System.out.println("Couleur de cheveux : "+this.hairColor);
+		System.out.println("Santé : "+this.health+" Vie");
+		System.out.println("Faim : "+this.hungerLevel);
+		System.out.println("Esperence de vie : "+ this.lifeExpectancy);
+		System.out.println("Fatigue : "+this.tiredness);
 	}
 	
+	public void riseDirtyLevel(int number) {
+		this.dirtLevel += number;
+	}
+	public void riseHungerLevel(int number) {
+		this.hungerLevel += number;
+	}
 	
+	public void riseHealth(int number) {
+		if((this.tamagotchiType.equals("Chat") || this.tamagotchiType.equals("Chien")) && this.health< this.HEALTHMAXCAT ) {
+			
+			this.health += number;
+		}
+		if(this.tamagotchiType.equals("Gragon")  && this.health< this.HEALTHMAXDRAGON ) {
+			this.health += number;
+		}
+		
+		
+	}
 	
+	public void decreasesHealth(int number) {
+		this.health -= number;
+	}
+	public void eat() {
+		if(hungerLevel>=50) {
+			setHungerLevel(0);
+			System.out.println("Votre tamagotchi n'a plus faim");
+		}else {
+			System.out.println("Votre tamagotchi n'a pas faim vous ne pouvez pas le nourir");
+		}
+		
+	}
+
+
+	public void goToToilet() {
+		riseDirtyLevel(5);
+		riseHungerLevel(2);
+	}
+	
+	public void rest() {
+		
+		riseHealth(1);
+		
+	}
+	
+	public void playSports() {
+		riseHealth(5);
+	}
+	
+	public void takeOffMask() {
+		this.masked = false;
+	}
+
+	public void wash() {
+		setDirtLevel(0);
+	}
+	public void die() {
+		setHealth(0);
+	}
+	public void smok() {
+		setHealth(0);
+	}
 }
